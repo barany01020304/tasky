7d9d8668033ab6405840f12507e7acedfda8c83c
BARANY <ahmedbrany01020304@gmail.com>
2026-02-21 11:32:37 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:device_frame/device_frame.dart';
+import 'package:flutter/material.dart';
+import 'package:tasky/screens/welcome_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:DeviceFrame(
+        device: Devices.android.samsungGalaxyNote20Ultra, // 👈 iPhone device frame
+        screen: Builder(
+          builder: (context) {
+            return WelcomeScreen();
+          },
+        ),
+      ),
+
+    );
+  }
+}
+
---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0XFF181818),
+      //  appBar: AppBar(backgroundColor: Color(0XFF181818)),
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset("assets/images/logo.svg"),
+                SizedBox(width: 16),
+                Text(
+                  "Tasky",
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0XFFFFFFFF),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 108),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Welcome To Tasky",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0XFFFFFFFF),
+                  ),
+                ),
+                SizedBox(width: 8),
+                SvgPicture.asset("assets/images/waving_hand.svg"),
+              ],
+            ),
+            SizedBox(height: 8),
+            Text(
+              "Your productivity journey starts here.",
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                color: Color(0XFFFFFFFF),
+                fontSize: 16,
+              ),
+            ),
+            SizedBox(height: 24,),
+            SvgPicture.asset("assets/images/welcome_image.svg"),
+            SizedBox(height: 24,),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: ElevatedButton(onPressed: (){},style: ElevatedButton.styleFrom(
+                fixedSize: Size(double.maxFinite, 42),
+                backgroundColor: Color(0XFF15B86C),
+                foregroundColor: Color(0XFFFFFFFF)
+
+              ),
+                  child: Text("Let’s Get Started")),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
